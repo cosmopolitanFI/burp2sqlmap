804a99aff2bb2c76db105b039f9bd3aec93a88a9
Walentin Helling <walentin.helling@nixu.com>
2022-03-24 11:40:48 +0200

    current

--- burp2sqlmap.py
+++ burp2sqlmap.py
@@ -16,7 +16,8 @@ def read_burp(file):
   else:
     print("File does not exist")
 
-def run_sqlmap(requests):
+def write_requests(requests):
+  os.makedirs("requests/", exist_ok=True)
   for req in requests:
     request = base64.b64decode(req.contents[0])
 
@@ -26,22 +27,42 @@ def run_sqlmap(requests):
 
     with open(file, "wb+") as f:
       f.write(request)
-    
 
-    alert = "echo {} >> vulnerable.log".format(req_id)
-    command = ["sqlmap","-r","requests/{}".format(req_id),
-               "--random-agent", "--threads=10", "--batch", "--flush-session",
-               "--answer='redirect=N'","--risk=3","--level=5","--alert={}".format(alert)]
-    subprocess.run(command)
+  print("[*] Written {} requests to disk".format(len(requests)))
+
+def run_sqlmap():
+  request_files = os.listdir("requests/")
+
+  for request in request_files:
+    if "scanned" not in request and "vulnerable" not in request:
+      alert = "mv requests/{0} requests/{0}-vulnerable".format(request)
+
+      command = ["python3","sqlmap-dev/sqlmap.py","-r","requests/{}".format(request),
+        "--proxy=http://127.0.0.1:8080", "--random-agent", "--threads=10", "--batch", 
+        "--flush-session","--answer='redirect=N'","--alert={}".format(alert)]
+      
+      print("[i] Running command: {}".format(command))
+      subprocess.run(command)
+
+      request = os.system("ls requests/ |grep {}".format(request))
+      if "vulnerable" not in request:
+        os.system("mv requests/{0} requests/{0}-scanned".format(request))
+
 
 if __name__ == "__main__":
   try:
     burp_file = sys.argv[1]
-    requests = read_burp(burp_file)
-    os.makedirs("requests/", exist_ok=True)
-    run_sqlmap(requests)
+    cont = False
+    if len(sys.argv) == 3 and sys.argv[2] == "continue":
+      cont = True
+
+    if not cont:
+      requests = read_burp(burp_file)
+      write_requests(requests)
+
+    run_sqlmap()
 
   except Exception as e:
     print("Error! - Example")
-    print("python3 burp2sqlmap.py BURPFILE")
+    print("python3 burp2sqlmap.py BURPFILE [continue]")
     print(e)
