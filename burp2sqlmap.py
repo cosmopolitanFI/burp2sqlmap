7a93139a9f712d327efd578cb5fcacdba61ab56e
Walentin Helling <walentin.helling@nixu.com>
2022-12-08 16:44:26 +0200

    improve it

--- burp2sqlmap.py
+++ burp2sqlmap.py
@@ -4,6 +4,7 @@ import sys
 import uuid
 import base64
 import shutil
+import argparse
 import subprocess
 
 from bs4 import BeautifulSoup
@@ -19,35 +20,81 @@ def read_burp(file):
   else:
     print("File does not exist")
 
+def update_log(file):
+  read_log = []
+  with open("requests/log.txt", "r") as f:
+    for line in f.readlines():
+      if file == line.strip():
+        line.replace("WAIT", "DONE")
+      read_log.append(line)
 
-def run_sqlmap(requests):
-  level = int(input("Intensity level (1-5): "))
-  risk = int(input("Risk level (1-3): "))
-  for req in requests:
-    request = base64.b64decode(req.contents[0])
+  with open("requests/log.txt", "w+") as f:
+    for line in read_log:
+      f.write(line)
 
-    # Write request to file
-    req_id = uuid.uuid4().hex
-    file = "requests/{}".format(req_id)
+def run_sqlmap(files, args):
+  for file in files:
+    if "WAIT" in file:
+      filename = file.split(" ")[0]
+      alert = "echo {} >> vulnerable.log".format(filename)
+      command = ["sqlmap","-r","requests/{}".format(filename), \
+                 "--level={}".format(args.intensity), \
+                 "--risk={}".format(args.risk),
+                 "--random-agent", \
+                 "--threads={}".format(args.threads), \
+                 "--batch", "--flush-session",
+                 "--answer='redirect=N'","--force-ssl", \
+                 "--alert={}".format(alert)]
+      if args.proxy != "":
+        command.append("--proxy={}".format(args.proxy))
+      subprocess.run(command)
+      update_log(file)
 
-    with open(file, "wb+") as f:
-      f.write(request)
+def store_requests(requests):
+  if os.path.exists("requests"):
+    shutil.rmtree("requests")
+  os.makedirs("requests/", exist_ok=True)
+  files = []
+  with open("requests/log.txt", "w+") as log:
+    for request in requests:
+      request = base64.b64decode(req.contents[0])
+      
+      # Write request to file
+      req_id = uuid.uuid4().hex
+      file = "requests/{}".format(req_id)
+      with open(file, "wb+") as f:
+        f.write(request)
+      file += " -- WAIT\n"
+      log.write(file) # keep track of file names
+      files.append(file.strip()) # Store file names to list
+  return files
 
-    alert = "echo {} >> vulnerable.log".format(req_id)
-    command = ["sqlmap","-r","requests/{}".format(req_id), "--level={}".format(level), "--risk={}".format(risk),
-               "--random-agent", "--threads=10", "--batch", "--flush-session",
-               "--answer='redirect=N'","--force-ssl","--proxy=http://localhost:8080/","--alert={}".format(alert)]
-    subprocess.run(command)
+def read_log():
+  files = []
+  with open("requests/log.txt", "r") as f:
+    for file in f.readlines():
+      files.append(file.strip())
+  return files
 
 
 if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Do sqlmap for burp file")
+  required = parser.add_argument_group("Required arguments")
+  parser.add_argument("--requests", action="store", dest="requests", help="requests.xml")
+  parser.add_argument("--intensity", default="1", action="store", dest="intensity")
+  parser.add_argument("--risk", default="1", action="store", dest="risk")
+  parser.add_argument("--proxy", default="", action="store", dest="proxy")
+  parser.add_argument("--threads", default="10", action="store", dest="threads")
+  parser.add_argument("--new", default=True, action="store", dest="new")
+  args = parser.parse_args()
   try:
-    burp_file = sys.argv[1]
+    burp_file = args.requests
     requests = read_burp(burp_file)
-    if os.path.exists("requests"):
-      shutil.rmtree("requests")
-    os.makedirs("requests/", exist_ok=True)
-    run_sqlmap(requests)
+    if args.new:
+      files = store_requests(requests)
+    else:
+      files = read_log()
+    run_sqlmap(files, args)
 
   except Exception as e:
     print("Error! - Example")
